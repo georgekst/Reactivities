aeb506d256efa875691d0e87ccb1dd78e1448864
George Kostopoulos <George.Kst@icloud.com>
2021-09-25 13:37:24 +0300

    Start of Section 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+import { List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./Navbar";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
