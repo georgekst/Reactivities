66a768a09e3efe6b6820f66945a1297a4fcd1ac1
George Kostopoulos <George.Kst@icloud.com>
2021-11-10 11:34:56 +0200

    End of section 12

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        // At least 1 uppercase, 1 lowercase, 1 number and 4 to 8 characters long
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
+        public string Password { get; set; }
+        [Required]
+        public string Username { get; set; }
+    }
+}
