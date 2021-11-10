955584f959fe2b8f9a354c1186cf948e7020b094
George Kostopoulos <George.Kst@icloud.com>
2021-11-10 15:29:13 +0200

    End of section 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -10,37 +10,56 @@ import TestErrors from "../../features/errors/TestError";
 import { ToastContainer } from "react-toastify";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
+import LoginForm from "../../features/users/LoginForm";
+import { useStore } from "../stores/store";
+import { useEffect } from "react";
+import LoadingComponent from "./LoadingComponent";
+import ModalContainer from "../common/modals/ModalContainer";
 
 function App() {
-  const location = useLocation();
-  return (
-    <>
-      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
-      <Route exact path="/" component={HomePage} />
-      <Route
-        path={"/(.+)"}
-        render={() => (
-          <>
-            <NavBar />
-            <Container style={{ marginTop: "7em" }}>
-              <Switch>
-                <Route exact path="/activities" component={ActivityDashboard} />
-                <Route path="/activities/:id" component={ActivityDetails} />
-                <Route
-                  key={location.key}
-                  path={["/createActivity", "/manage/:id"]}
-                  component={ActivityForm}
-                />
-                <Route path="/errors" component={TestErrors} />
-                <Route path="/server-error" component={ServerError} />
-                <Route component={NotFound} />
-              </Switch>
-            </Container>
-          </>
-        )}
-      />
-    </>
-  );
+    const location = useLocation();
+    const { commonStore, userStore } = useStore();
+
+    useEffect(() => {
+        if (commonStore.token) {
+            userStore.getUser().finally(() => commonStore.setAppLoaded());
+        } else {
+            commonStore.setAppLoaded();
+        }
+    }, [commonStore, userStore]);
+
+    if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
+
+    return (
+        <>
+            <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
+            <ModalContainer />
+            <Route exact path="/" component={HomePage} />
+            <Route
+                path={"/(.+)"}
+                render={() => (
+                    <>
+                        <NavBar />
+                        <Container style={{ marginTop: "7em" }}>
+                            <Switch>
+                                <Route exact path="/activities" component={ActivityDashboard} />
+                                <Route path="/activities/:id" component={ActivityDetails} />
+                                <Route
+                                    key={location.key}
+                                    path={["/createActivity", "/manage/:id"]}
+                                    component={ActivityForm}
+                                />
+                                <Route path="/errors" component={TestErrors} />
+                                <Route path="/server-error" component={ServerError} />
+                                <Route path="/login" component={LoginForm} />
+                                <Route component={NotFound} />
+                            </Switch>
+                        </Container>
+                    </>
+                )}
+            />
+        </>
+    );
 }
 
 export default observer(App);
